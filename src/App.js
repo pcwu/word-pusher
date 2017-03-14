161e5119904d30012dc527464054559cedb3d675
Michelle Wu <snoopy4265@gmail.com>
2017-03-14 21:31:04 +0100

    Add Stepper.js and add composing.css to rearrange css style

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 import Home from './components/Home';
+import Stepper from './components/Stepper';
 
 const muiTheme = getMuiTheme({
   palette: {
     primary1Color: indigoA700,
     primary2Color: blueA200,
     accent1Color: deepOrange500,
-    textColor: white,
   },
 });
 
 const App = () => (
   <MuiThemeProvider muiTheme={muiTheme}>
-    <Home />
+    <div>
+      <Home />
+      <Stepper />
+    </div>
   </MuiThemeProvider>
 );
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
-import './Home.css';
+import './composing.css';
+
+const bgStyle = {
+  background: '#304FFE',
+};
 
 const Home = () => (
-  <div className="entire-wrapper">
-    <div className="content-wrapper">
-      <h1 style={{ color: '#fff', fontWeight: 300 }}>Suffer from memorizing vocabulary?</h1>
-      <h3 style={{ color: '#fff', fontWeight: 300 }}>Let us help you.</h3>
+  <div className="entire-wrapper" style={bgStyle}>
+    <div className="content-wrapper padding-sm">
+      <h1>Suffer from memorizing vocabulary?</h1>
+      <h3>Let us help you.</h3>
       <RaisedButton label="Explore Now" secondary />
     </div>
   </div>

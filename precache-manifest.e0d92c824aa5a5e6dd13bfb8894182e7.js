fe1d5327448991693c732c152e94871ad3684ba3
Po-Chun Wu <sb1126@gmail.com>
2019-02-19 23:55:50 +0100

    Updates

---
+++ precache-manifest.e0d92c824aa5a5e6dd13bfb8894182e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0591cb08516d2edee5bb",
+    "url": "/word-pusher/static/css/main.8e0ace66.chunk.css"
+  },
+  {
+    "revision": "0591cb08516d2edee5bb",
+    "url": "/word-pusher/static/js/main.0591cb08.chunk.js"
+  },
+  {
+    "revision": "39fcd79e56a8802764b7",
+    "url": "/word-pusher/static/js/runtime~main.39fcd79e.js"
+  },
+  {
+    "revision": "196412e27d636bb8a87e",
+    "url": "/word-pusher/static/js/2.196412e2.chunk.js"
+  },
+  {
+    "revision": "938b2e2ceadd4cc49129ee3a753db188",
+    "url": "/word-pusher/index.html"
+  }
+];
